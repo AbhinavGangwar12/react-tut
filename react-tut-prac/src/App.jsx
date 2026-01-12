4efb5c66e1751fd125d890d5cff1b979507de27b
AbhinavGangwar12 <abhinav1203gangwar@gmail.com>
2026-01-12 11:06:22 +0530

    first interaction with react

---
+++ react-tut-prac/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+import './App.css'
+import Coffee from './Coffee'
+function App() {
+
+  return (<Coffee />)
+}
+
+export default App
---
+++ react-tut-prac/src/Coffee.jsx
@@ -0,0 +1,5 @@
+function coffee(){
+    return <h2>Coffee is brewed</h2>
+}
+
+export default coffee
