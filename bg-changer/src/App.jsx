406b9895d9d5d48ddf4e5813157482a1504c3716
AbhinavGangwar12 <abhinav1203gangwar@gmail.com>
2026-01-13 17:55:22 +0530

    bg-color-changer

--- bg-changer/src/App.jsx
+++ bg-changer/src/App.jsx
@@ -2,11 +2,22 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [color, setColor] = useState("red")
+  const [color, setColor] = useState("bg-white")
 
   return (
     <>
-      <div className="w-screen h-full" script={{backgroundColor : color}}></div>
+      <div className={`w-full h-screen border-0 ${color}`}>
+        <div className="fixed flex flex-wrap items-center inset-x-0 justify-center bottom-12">
+          <div className="flex flex-wrap justify-center gap-3 bg-white pt-1.5 pb-1.5 pl-1 pr-1 rounded-2xl">
+            <button className="bg-red-500 text-amber-50 font-bold rounded-xl w-20 h-8" onClick={()=>setColor("bg-red-500")}>Red</button>
+            <button className="bg-black text-amber-50 font-bold rounded-xl w-20 h-8" onClick={()=>setColor("bg-black")}>Black</button>
+            <button className="bg-yellow-300 text-amber-50 font-bold rounded-xl w-20 h-8" onClick={()=>setColor("bg-yellow-300")}>Yellow</button>
+            <button className="bg-yellow-950 text-amber-50 font-bold rounded-xl w-20 h-8" onClick={()=>setColor("bg-yellow-950")}>Brown</button>
+            <button className="bg-blue-500 text-amber-50 font-bold rounded-xl w-20 h-8" onClick={()=>setColor("bg-blue-500")}>Blue</button>
+          </div>
+        </div>
+
+      </div>
     </>
   )
 }
